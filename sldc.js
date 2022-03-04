c4028db5f778f09c2bbd735fd458ca0424029750
Deondre Hambric <dhambric1@gmail.com>
2022-03-04 10:33:52 -0700

    SLDC answers

--- sldc.js
+++ sldc.js
@@ -20,7 +20,7 @@
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
 const Analysis = "The analysis stage includes gathering all the specific details required for a new system as well as determining the first ideas for prototypes"
-const Design = " The design stage is a necessary precursor to the main devloper stage"
+const Design = "The design stage is a necessary precursor to the main devloper stage."
 const testing = 'During testing stage, developers will go over software noting any bugs or defects that need to be tracked, fixed, and later retested. The information system will be integrated into is enviroment and eventually installed'
 const Implementation = "After testing the overall design for the software will come together. Different modules or designs will be intergrated into the primary source code through developers efforts"
 const Maintain = 'Developers must now move into a maintenance mode and begin practicing any activites required to handle issues reported by end-users'
